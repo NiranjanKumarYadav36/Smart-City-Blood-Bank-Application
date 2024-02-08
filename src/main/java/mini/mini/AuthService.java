f120ce66b7f7cb193a9baee1810c93bae70ecebd
Niranjan Kumar Yadav <140634765+NiranjanKumarYadav36@users.noreply.github.com>
2024-02-08 19:41:31 +0530

    Update AuthService.java

--- src/main/java/mini/mini/AuthService.java
+++ src/main/java/mini/mini/AuthService.java
@@ -1,49 +1,53 @@
 package mini.mini;
 
-    public class AuthService {
-        private static AuthService instance;
-        private int loggedInUserId = -1; // Default value when no user is logged in
+// AuthService class manages user authentication and session
+public class AuthService {
+    private static AuthService instance; // Singleton instance of AuthService
+    private int loggedInUserId = -1; // Default value when no user is logged in
 
-        private String loggedINUsername;
-        private String loggedInUsername;
+    // Fields to store logged-in user information
+    private String loggedINUsername; // Username of the logged-in user
+    private String loggedInUsername; // Username of the logged-in user
 
-        private AuthService() {
-            // Private constructor to prevent direct instantiation
-        }
-
-        public static AuthService getInstance() {
-            if (instance == null) {
-                instance = new AuthService();
-            }
-            return instance;
-        }
-
-        public void login(int userId, String loggedInUsername) {
-            loggedInUserId = userId;
-        }
-
-        public void logout() {
-            loggedInUserId = -1;
-        }
-
-        public int getLoggedInUserId() {
-            return loggedInUserId;
-        }
-
-
-        public String getLoggedINUsername(){
-                return loggedINUsername;
-        }
-
-        public String getLoggedInUsername() {
-            return loggedINUsername;
-        }
-
-        public void setLoggedInUsername(String loggedInUsername) {
-            this.loggedInUsername = loggedInUsername;
-        }
-
+    // Private constructor to prevent direct instantiation
+    private AuthService() {
     }
 
+    // Method to get the singleton instance of AuthService
+    public static AuthService getInstance() {
+        if (instance == null) {
+            instance = new AuthService();
+        }
+        return instance;
+    }
 
+    // Method to log in a user
+    public void login(int userId, String loggedInUsername) {
+        loggedInUserId = userId;
+    }
 
+    // Method to log out a user
+    public void logout() {
+        loggedInUserId = -1;
+    }
+
+    // Method to get the ID of the logged-in user
+    public int getLoggedInUserId() {
+        return loggedInUserId;
+    }
+
+    // Getter method for the loggedINUsername field
+    public String getLoggedINUsername() {
+        return loggedINUsername;
+    }
+
+    // Getter method for the loggedInUsername field
+    public String getLoggedInUsername() {
+        return loggedInUsername;
+    }
+
+    // Setter method for the loggedInUsername field
+    public void setLoggedInUsername(String loggedInUsername) {
+        this.loggedInUsername = loggedInUsername;
+    }
+}
